c17f7b67140b901559473a2edaa30128197068e9
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2024-12-05 01:06:21 -0300

    generate a different opengraph-image for each product

---
+++ src/app/(store)/product/[slug]/opengraph-image.tsx
@@ -0,0 +1,67 @@
+import { ImageResponse } from 'next/og'
+import colors from 'tailwindcss/colors'
+
+import { api } from '@/data/api'
+import { Product } from '@/data/types/product'
+import { env } from '@/env'
+
+export const runtime = 'edge'
+
+// Image metadata
+export const alt = ''
+
+export const size = {
+  width: 1200,
+  height: 630,
+}
+
+export const contentType = 'image/png'
+
+async function getProduct(slug: string): Promise<Product> {
+  const response = await api(`/products/${slug}`, {
+    next: {
+      revalidate: 60 * 15, // 15 minutes
+    },
+  })
+
+  const product = await response.json()
+
+  return product
+}
+
+export default async function Image({
+  params,
+}: {
+  params: Promise<{
+    slug: string
+  }>
+}) {
+  const { slug } = await params
+  const product = await getProduct(slug)
+
+  const productImageURL = new URL(product.image, env.APP_URL).toString()
+
+  /* eslint-disable @next/next/no-img-element */
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          background: colors.zinc[950],
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <img src={productImageURL} alt="" style={{ width: '100%' }} />
+      </div>
+    ),
+    // ImageResponse options
+    {
+      // For convenience, we can re-use the exported opengraph-image
+      // size config to also set the ImageResponse's width and height.
+      ...size,
+    },
+  )
+  /* eslint-enable @next/next/no-img-element */
+}
