28f38a1398043d9a91572cc1836268ae8c9e1e31
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2024-12-07 03:59:22 -0300

    change component name and add explicit null fallback

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -2,17 +2,14 @@ import { Suspense } from 'react'
 
 import { Skeleton } from '@/components/skeleton'
 
-import { ProductBeingSearch } from './product-being-search'
+import { CurrentSearch } from './current-search'
 
 export default function SearchLoading() {
   return (
     <div className="flex flex-col gap-4">
-      <p className="text-sm">
-        Resultados para:{' '}
-        <Suspense>
-          <ProductBeingSearch />
-        </Suspense>
-      </p>
+      <Suspense fallback={null}>
+        <CurrentSearch />
+      </Suspense>
       <div className="grid grid-cols-3 gap-6">
         <Skeleton className="h-[400px]" />
         <Skeleton className="h-[400px]" />
--- src/components/search-form.tsx
+++ src/components/search-form.tsx
@@ -30,7 +30,7 @@ export function SearchForm() {
       className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700"
     >
       <Search className="h-5 w-5 text-zinc-500" />
-      <Suspense>
+      <Suspense fallback={null}>
         <SearchQueryInput />
       </Suspense>
     </form>
