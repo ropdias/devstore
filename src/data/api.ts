fe75d82235ce06466de836949595de7a31a8b19f
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-12-01 11:24:50 -0300

    fix: simplify base URL handling in API function by removing VERCEL_URL check

--- src/data/api.ts
+++ src/data/api.ts
@@ -1,9 +1,7 @@
 import { env } from '@/env'
 
 export function api(path: string, init?: RequestInit) {
-  const baseUrl = process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
-    : env.NEXT_PUBLIC_API_BASE_URL
+  const baseUrl = env.NEXT_PUBLIC_API_BASE_URL
   const apiPrefix = '/api'
   const url = new URL(apiPrefix.concat(path), baseUrl)
 
